d556c47dc7a40ae90c8624d442fba540725f2d8e
kartikeya tiwari <kartikeytiwari79.kt@gmail.com>
2018-02-14 00:43:59 +0530

    add GET todos route

--- server/server.js
+++ server/server.js
@@ -18,6 +18,13 @@ todo.save().then((doc) => {
   res.status(400).send(e)
 });
 });
+app.get('/todos',(req,res) => {
+  Todo.find().then((todos) => {
+    res.send({todos})
+  })
+},(e) => {
+  res.status(400).send('unable to get the todos',e)
+})
 
 app.listen(3000,()=>{
   console.log('the app is running on port 3000');
