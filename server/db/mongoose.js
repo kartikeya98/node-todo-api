1da271a0a17af29c42cf6d2903ff48369d15b1a0
kartikeya tiwari <kartikeytiwari79.kt@gmail.com>
2018-02-15 23:47:01 +0530

    seeting up api to heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,12 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-
-let db = {
-  localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://kartikeya9_8:z-star@kt@ds027335.mlab.com:27335/node-todo-98'
-};
-mongoose.connect( process.env.PORT ? db.mlab : db.localhost);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 
 
