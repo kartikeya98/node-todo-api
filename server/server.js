ae55c4f77aedb487a2707351f2451c488db97318
kartikeya tiwari <kartikeytiwari79.kt@gmail.com>
2018-02-16 02:28:16 +0530

    added the update route

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const _ = require('lodash');
 var {ObjectID} =require('mongodb');
 var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
@@ -61,7 +62,29 @@ app.delete('/todos/:id',(req,res) => {
     res.status(404).send()
   });
 });
+app.patch('/todos/:id',(req,res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body,['text','completed']);
 
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if(_.isBoolean(body.completed) && body.completed===true) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+  Todo.findByIdAndUpdate(id ,{$set:body},{new:true}).then((todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
+});
 app.listen(Port,()=> {
   console.log(`the app is running on ${Port}`);
 })
