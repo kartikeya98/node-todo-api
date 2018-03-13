9e54e5927755b60c676620bb447fe022380f1836
kartikeya tiwari <kartikeytiwari79.kt@gmail.com>
2018-03-13 22:05:36 +0530

    hash user password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,27 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
-var data = {
-  id:10
-};
+const bcrypt = require('bcryptjs');
 
-var token = jwt.sign(data , '123abc');
-console.log(token);
+// var password = '123abc!'
+//  bcrypt.genSalt(10,(err,salt) =>{
+//    bcrypt.hash(password,salt,(err,hash) => {
+//      console.log(hash);
+//    })
+//  })
+ var hashpassword = '$2a$10$cv8kMSmeqazAhVm3oweqxuesGqtz0xr7XQIZPR3tvnwCsJc5EfaGm';
 
-var decoded = jwt.verify(token,'123abc');
-console.log('decoded',decoded);
+ bcrypt.compare(password,hashpassword,(err,res) => {
+   console.log(res);
+ })
+// var data = {
+//   id:10
+// };
+//
+// var token = jwt.sign(data , '123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token,'123abc');
+// console.log('decoded',decoded);
 
 // var message = 'i am user number 3';
 // var hash = SHA256(message).toString();
