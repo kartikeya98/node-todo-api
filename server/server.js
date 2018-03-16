9523033f2a6bc599dd2ddbc413563f01f1ab5046
kartikeya tiwari <kartikeytiwari79.kt@gmail.com>
2018-03-16 21:10:20 +0530

    added login system in users

--- server/server.js
+++ server/server.js
@@ -114,6 +114,31 @@ app.get('/users/me',authenticate,(req,res) => {
   });
 });
 
+// POST /users/login {email, password}
+// app.post('/users/login', (req, res) => {
+//   var body = _.pick(req.body,['email', 'password']);
+//
+//   User.findByCredentials(body.email, body.password).then((user) => {
+//     return user.generateAuthToken().then((token) => {
+//       res.header('x-auth', token).send(user);
+//     });
+//   }).catch((e) => {
+//     res.status(400).send();
+//   });
+// });
+
+app.post('/users/login',(req,res) => {
+var body = _.pick(req.body,['email','password']);
+
+  User.findByCredentials(body.email,body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+    res.header('x-auth', token).send(user);
+  });
+  }).catch((e) => {
+    res.status(400).send(e)
+  });
+});
+
 app.listen(Port,()=> {
   console.log(`the app is running on ${Port}`);
 })
