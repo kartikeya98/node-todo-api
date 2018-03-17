5531f7b483a1198719a5c3dc228df609008edb73
kartikeya tiwari <kartikeytiwari79.kt@gmail.com>
2018-03-18 01:05:45 +0530

    upgraded the expect version

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -139,7 +139,7 @@ request(app)
     return done(err);
   }
 Todo.findById(hexId).then((todo) => {
-  expect(todo).toExist();
+  expect(todo).toBeTruthy();
   done();
 }).catch((e) => done(e));
 });
@@ -177,7 +177,8 @@ describe('PATCH /todos/:id',() => {
     .expect((res) => {
       expect(res.body.todo.text).toBe(text);
       expect(res.body.todo.completed).toBe(true);
-      expect(res.body.todo.completedAt).toBeA ('number');
+      //expect(res.body.todo.completedAt).toBeA ('number');
+      expect(typeof res.body.todo.completedAt).toBe('number');
     })
     .end(done);
   });
@@ -251,8 +252,8 @@ describe('POST /users',() => {
     .send({email, password})
     .expect(200)
     .expect((res) => {
-      expect(res.headers['x-auth']).toExist()
-      expect(res.body._id).toExist();
+      expect(res.headers['x-auth']).toBeTruthy()
+      expect(res.body._id).toBeTruthy();
       expect(res.body.email).toBe(email);
     })
     .end((err) => {
@@ -260,8 +261,8 @@ describe('POST /users',() => {
         return done(err);
       }
       User.findOne({email}).then((user) => {
-        expect(user).toExist();
-        expect(user.password).toNotBe(password);
+        expect(user).toBeTruthy();
+        expect(user.password).not.toBe(password);
         done();
       }).catch((e) => done(e));
     });
@@ -300,7 +301,7 @@ describe('POST /users/login', () => {
     })
     .expect(200)
     .expect((res) => {
-      expect(res.headers['x-auth']).toExist();
+      expect(res.headers['x-auth']).toBeTruthy();
     })
     .end((err,res) => {
       if(err) {
@@ -308,7 +309,7 @@ describe('POST /users/login', () => {
       }
 
       User.findById(users[1]._id).then((user) => {
-        expect(user.tokens[1]).toInclude({
+        expect(user.toObject().tokens[1]).toMatchObject({
           access: 'auth',
           token: res.headers['x-auth']
         });
@@ -326,7 +327,7 @@ describe('POST /users/login', () => {
     })
     .expect(400)
     .expect((res) => {
-      expect(res.headers['x-auth']).toNotExist();
+      expect(res.headers['x-auth']).toBeFalsy();
     })
     .end((err,res) => {
       if(err) {
